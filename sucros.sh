a6329b09a6aa63f8c180e192b81f5d2da905beed
shadowed1 <itszachg@gmail.com>
2026-01-14 17:28:44 -0500

    Update sucros.sh

--- sucros.sh
+++ sucros.sh
@@ -5,12 +5,12 @@
 FIFO="/home/chronos/.sucros.fifo"
 
 if [[ ! -p "$FIFO" ]]; then
-    echo "sucros: daemon not running" >&2
+    echo "sucros-daemon not running" >&2
     exit 1
 fi
 
 if [[ $# -eq 0 ]]; then
-    echo "usage: sucros <command...>" >&2
+    echo "usage: sucros command" >&2
     exit 1
 fi
 
