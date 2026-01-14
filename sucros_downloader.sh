7bfb48ad6c191b34af9e84f1745f4c0a28f27881
shadowed1 <itszachg@gmail.com>
2026-01-14 17:42:16 -0500

    Update sucros_downloader.sh

--- sucros_downloader.sh
+++ sucros_downloader.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 # Sucros Downloader
 # shadowed1
-
+echo
+echo ""
+echo "                                                           .lxo. "                                    
+echo "                                                 .';,.     .oOx' "                                    
+echo "                                                 ;x00,      'c. "                                     
+echo "                                                 .cl;       ,:. "                                     
+echo "                                                .;c;.     .'cc.    ..  .';:;..    .:dd:. "            
+echo "                                               .;cc:'.....;oxxo,,;cxOc.':clc;',:c:cOXN0, "            
+echo "                                                .....     .;lc'...':l, .';;'.     .;ll;. "            
+echo "                                                           .c'          .'. "                         
+echo "                                                           .:.          .,. "                         
+echo "                                      ...                  .c.          .;. "                         
+echo "                           .ol.       ;Ok'       ...      .:l:.        .:c'     ... "               
+echo "           ',.          .:;;cc:;.....,:;';oc.   :OK0o:::,;lxxdc,''''.';oxxd:,,:lx0O; "              
+echo "          .x0c          lK0:   .,:llc,   ;d,    ,oxd:'....':l:........,lddo;..'':ol. "                
+echo "           ;c.          :d,      ,oo:'. .'.                .,.         .cc. "                      
+echo "         .,'          .l,       ',.  .';,.                .'.         .c; "                         
+echo "         .:c;'.......,:l:.      .,.     ..                 ';,.        .c; "                          
+echo "          .'......'';oxdc'..  .';.                        .;cc'        .c; "                         
+echo "                     .;c'....'cdl.                         ',.         .c;. "                         
+echo "                      .;.    ,:c:,,.                       ,'.,,.    .,lool' "                       
+echo "       .ldc'          .:. ,lo,    .......                 .:,oKX0ocl::loxxxl;,'...     .... "        
+echo "       ;0XOoccc,..',,..;' ;dc.       .'::;.  .           .;;.;xOd,.....:lc:...''...'..';:c:, "       
+echo "        .,'...'..,:cc;,;:,.           .''...,:;'........;ldo,. .      .;'            .';ccc,. "      
+echo "                  .,,,:clc,''..... ..','...';c:,......'':oddc,'...   .';.              .;;.. "     
+echo "                    ':;,,. ....''',cdxo:,..  ..          ;c.  .',,'.;clc'...,ldc.      .c; "      
+echo "                 .,ll.             'ldl'                :kx.       'ldxxl,,:cd0x'      .kd. "        
+echo "               .l0Ko.               .:,                 ;do.        'll;.     .        'xo. "       
+echo "                'cc.                 ;;                             .c'               ,d0Kk; "       
+echo "                                     ,:                             ':.               'o0KO; "      
+echo "                                     ':.                            ',                 ..'. "      
+echo "                                    .:l:.                          .,' "                           
+echo "                              .;c;,,cddd:'''....',,'.             .','. "                            
+echo "                              .xOl'..:;'.  .....;cc;.            .,ccc,. "                           
+echo "                               ..   ':.         .;;.             .;cc,. "                          
+echo "                                   l0Oc         .od,            .l00o. "                           
+echo "                                   ,ol'         cO0o.            .:c' "                            
+echo "                                                :kkl. "                             
+echo
+echo "                                                Sucros"
+echo
 curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros_installer.sh -o /home/chronos/sucros_installer
 echo
 echo "Run the commands listed below in VT-2 to continue the installer:"
