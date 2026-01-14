3a019b44b0bbf62d2616e8b63b0926c1a5cebffc
shadowed1 <itszachg@gmail.com>
2026-01-14 16:01:02 -0500

    Update elevate.sh

--- elevate.sh
+++ elevate.sh
@@ -2,9 +2,6 @@
 # ChromeOS Elevated Shell
 # shadowed1
 
-ELEVATE_DIR="/usr/local/"
-FIFO="$ELEVATE_DIR/.elevate.fifo"
-
 if [[ -f /tmp/machine-info ]]; then
     DEVICE_NAME=$(grep '^customization_id=' /tmp/machine-info 2>/dev/null | cut -d= -f2 | tr -d '"')
 else
@@ -19,14 +16,14 @@ else
 fi
 
 USER_NAME=${USER:-$(whoami)}
+
 BLUE=$(tput setaf 4)
+BOLD_BLUE=$(tput bold; tput setaf 4)
 BOLD_GREEN=$(tput bold; tput setaf 2)
 RESET=$(tput sgr0)
-ORIGINAL_PS1="${BOLD_GREEN}$USER_NAME@$DEVICE_NAME-$BOARD_REV${RESET} \w # "
-ELEVATED_PS1="${BLUE}[elevated]${RESET} ${BOLD_GREEN}$USER_NAME@$DEVICE_NAME-$BOARD_REV${RESET} \w # "
 
-export ORIGINAL_PS1
+ORIGINAL_PS1="${BOLD_GREEN}$USER_NAME@$DEVICE_NAME-$BOARD_REV${RESET}${BOLD_BLUE} \w $ ${RESET}"
+ELEVATED_PS1="${BLUE}[sudo enabled]${RESET} ${BOLD_GREEN}$USER_NAME@$DEVICE_NAME-$BOARD_REV${RESET}${BOLD_BLUE} \w $ ${RESET}"
+
 export PS1="$ELEVATED_PS1"
-script -qfc "export PS1=\"\$PS1\"; exec bash --norc -i" /dev/null
-echo -ne "$RESET"
-export PS1="$ORIGINAL_PS1"
+exec bash --norc -i
