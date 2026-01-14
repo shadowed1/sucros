c181e59ccce4026ad7ef1f44dbc8db4e5bb8611a
shadowed1 <itszachg@gmail.com>
2026-01-14 18:37:49 -0500

    Update and rename sucros_installer.sh to bin/sucros_installer.sh

---
+++ bin/sucros_installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# sucros installer
+# shadowed1
+
+set -e
+echo "[sucros] Installing..."
+BIN_DIR="/usr/local/bin"
+INIT_DIR="/etc/init"
+mkdir -p "$BIN_DIR"
+
+curl -fsSL https://raw.githubusercontent.com/shadowed1/sucros/main/bin/sucros.sh -o "$BIN_DIR/sucros"
+curl -fsSL https://raw.githubusercontent.com/shadowed1/sucros/main/bin/sucros-daemon.sh -o "$BIN_DIR/sucros-daemon"
+curl -fsSL https://raw.githubusercontent.com/shadowed1/sucros/main/bin/sucros.conf -o "$INIT_DIR/sucros.conf"
+
+chmod 755 "$BIN_DIR/sucros"
+chmod 755 "$BIN_DIR/sucros-daemon"
+
+echo "[sucros] Installation complete"
+echo "[sucros] Run: sudo sucros-daemon in VT-2 logged in as chronos"
