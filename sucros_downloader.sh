5a12eca56d4851b710212588e61dc1bf736a983f
shadowed1 <itszachg@gmail.com>
2026-01-14 18:33:04 -0500

    Update sucros_downloader.sh

--- sucros_downloader.sh
+++ sucros_downloader.sh
@@ -42,12 +42,9 @@ echo "                                                :kkl. "
 echo
 echo "                                                Sucros"
 echo
-curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros_installer.sh -o /home/chronos/sucros_installer
+curl -L https://raw.githubusercontent.com/shadowed1/sucros/main/sucros_installer.sh -o /home/chronos/user/sucros_installer
 echo
 echo "Run the commands listed below in VT-2 to continue the installer:"
 echo "ctrl-alt-refresh to open VT-2 (ctrl-alt-back to exit) - log in as chronos"
 echo ""
-echo "sudo mv /home/chronos/sucros_installer /usr/local/"
-echo "sudo chmod +x /usr/local/sucros_installer"
-echo "sudo /usr/local/sucros_installer"
-echo
+echo ""
