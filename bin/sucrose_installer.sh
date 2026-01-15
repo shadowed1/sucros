10e3f8d420d903817fd38002a70407111f82a94f
shadowed1 <itszachg@gmail.com>
2026-01-14 23:12:13 -0500

    Update sucrose_installer.sh

--- bin/sucrose_installer.sh
+++ bin/sucrose_installer.sh
@@ -48,7 +48,7 @@ read -r -p "${BLUE}${BOLD}Start sucros-daemon now? [y/N] ${RESET}" ans
 
 if [[ "$ans" =~ ^[Yy]$ ]]; then
         echo "${BLUE}Running: sudo sucrose-daemon 2>/dev/null & ${RESET}"
-        sudo sucros-daemon 2>/dev/null &
+        sudo sucrose-daemon 2>/dev/null &
 else
         echo "${BLUE}Run: sudo sucrose-daemon in VT-2 logged in as chronos when ready ${RESET}"
 fi
